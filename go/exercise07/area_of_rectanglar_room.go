4a44c83dc60c9c71e8e93b738e169c11341d7e6d
sparrowsl <benthorpe.dev@gmail.com>
2023-12-16 10:22:21 +0000

    feat: add exercise 07

---
+++ go/exercise07/area_of_rectanglar_room.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("What is the length of room in feet? ")
+	scanner.Scan()
+	length, _ := strconv.Atoi(scanner.Text())
+
+	fmt.Print("What is the width of room in feet? ")
+	scanner.Scan()
+	width, _ := strconv.Atoi(scanner.Text())
+
+	fmt.Printf("You entered dimensions of %v feet by %v feet.\n", length, width)
+	fmt.Printf("The area is\n%v square feet\n%v square meters\n", length*width, (float64(length) * float64(width) * 0.09290304))
+}
