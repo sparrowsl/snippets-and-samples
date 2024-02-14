ea0c1393bf6a3dace91e163fc080c591b0c6e3a8
sparrowsl <benthorpe.dev@gmail.com>
2024-02-14 15:48:54 +0000

    add functionality to count words

--- go/word_counter/main.go
+++ go/word_counter/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+
+	scanner.Split(bufio.ScanWords)
+
+	wordCount := 0
+
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	return wordCount
 }
