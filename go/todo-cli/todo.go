743fe2fa6f18a0bcce398c7112e5cdd5d9ffc01d
sparrowsl <benthorpe.dev@gmail.com>
2024-02-15 12:17:38 +0000

    add documentation for methods

--- go/todo-cli/todo.go
+++ go/todo-cli/todo.go
@@ -8,12 +8,17 @@ type item struct {
 
 type List []item
 
+// Marks a to-do item as completed
 func (l List) Complete() {}
 
+// Creates a new todo and append it to the list
 func (l List) Add() {}
 
+// Deletes a to-do item from the list
 func (l List) Delete(id int) {}
 
+// Saves the list of items to a file using the JSON format
 func (l List) Save() {}
 
+// Obtains a list of items from a saved JSON file
 func (l List) Get() {}
