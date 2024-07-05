a337f3c4b5c7f329c3ab23b52eb6ed1054d8452f
sparrowsl <benthorpe.dev@gmail.com>
2024-07-05 10:51:11 +0000

    fix ex14 crash

--- javascript/exercise14/tax_calculator.js
+++ javascript/exercise14/tax_calculator.js
@@ -8,9 +8,9 @@ const state = await input("What is the state? ")
 // Check the amount and state condition
 if (state.toUpperCase() == "WI") {
   console.log(`The subtotal is $${amount.toFixed(2)}`)
-  console.log(`The tax is $${((subtotal * tax) / 100).toFixed(2)}`)
-  console.log(`The total is $${(subtotal + ((subtotal * tax) / 100)).toFixed(2)}`)
+  console.log(`The tax is $${((amount * tax) / 100).toFixed(2)}`)
+  console.log(`The total is $${(amount + ((amount * tax) / 100)).toFixed(2)}`)
   process.exit()
 }
 
-console.log(`The total is $${subtotal.toFixed(2)}`)
+console.log(`The total is $${amount.toFixed(2)}`)
