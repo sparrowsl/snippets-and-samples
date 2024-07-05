f2f80adbc9a0de8ef85b9bd8bcb7c0301f479043
sparrowsl <benthorpe.dev@gmail.com>
2024-07-05 10:54:55 +0000

    add ex15 js

---
+++ javascript/exercise15/password_validation.js
@@ -0,0 +1,9 @@
+import { input } from "pynput"
+
+const password = await input("What is the password? ")
+
+if (password === "abc$123") {
+  console.log("Welcome!")
+} else {
+  console.log("I don't know you.")
+}
