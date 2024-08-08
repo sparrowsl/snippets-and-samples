63d889dbd11fbb380e7c238f2214bf0727bb854b
sparrowsl <benthorpe.dev@gmail.com>
2024-08-08 15:55:11 +0000

    feat: read from a file

--- python/exercise34/employee_list_removal.py
+++ python/exercise34/employee_list_removal.py
@@ -1,22 +1,22 @@
-employees = [
-    "John Smith",
-    "Jackie Jackson",
-    "Chris Jones",
-    "Amanda Cullen",
-    "Jeremy Goodwin",
-]
+with open("./employees.txt") as file:
+    employees = [file.strip("\n") for file in file.readlines()]
+
 
 print(f"There are {len(employees)} employees:")
 for emp in employees:
-    print(f"  - {emp}")
+    print(f"  - {emp.strip()}")
+
 
 name = input("\nEnter an employee name to remove: ")
-
 if name not in employees:
     print(f"{name} is not an employee...")
     exit(0)
+
 employees.remove(name)
 
 print(f"\nThere are {len(employees)} employees:")
 for emp in employees:
     print(f"  - {emp}")
+
+with open("./employees.txt", "w") as file:
+    file.writelines(employees)
