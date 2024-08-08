8da7319f8e818b6be2f85103c37fadcac67d8e4f
sparrowsl <benthorpe.dev@gmail.com>
2024-08-08 17:10:23 +0000

    add ex39

---
+++ python/exercise39/sorting_records.py
@@ -0,0 +1,31 @@
+import sqlite3
+from typing import Any
+
+
+# Create the db
+with sqlite3.connect("./employees.db") as connection:
+    cursor = connection.cursor()
+
+employees: list[dict[str, Any]] = []
+
+# execute query
+rows = cursor.execute("SELECT * FROM employees").fetchall()
+
+
+for name, surname, position, date in rows:
+    employees.append(
+        {
+            "name": name,
+            "surname": surname,
+            "position": position,
+            "date": date,
+        }
+    )
+
+# sorted(employees, key=lambda employee: employee["surname"])
+employees.sort(key=lambda emp: emp["surname"])
+
+print(f"{'Name':12}| {'Position':12}| Separation Date")
+print(f"{'-'*12}| {'-'*12}| {'-'*15}")
+for emp in employees:
+    print(f"{emp["name"]:12}| {emp["surname"]:12}| {emp["date"] or ''}")
