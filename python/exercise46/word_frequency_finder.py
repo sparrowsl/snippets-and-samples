61fd616d48e2acb14a869ce9b7b76e00c0805af0
sparrowsl <benthorpe.dev@gmail.com>
2024-09-17 18:40:17 +0000

    feat: render template

--- python/exercise46/word_frequency_finder.py
+++ python/exercise46/word_frequency_finder.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -23,4 +23,4 @@ with open("./words.txt") as file:
 @app.get("/")
 def index():
     count_words(splitted)
-    return "<h1>Hello again Flask!!</h1>"
+    return render_template("index.html", name="Test")
