bbae2156e63510c538f8d0069333131833e9b5c2
sparrowsl <benthorpe.dev@gmail.com>
2024-09-19 17:30:06 +0000

    feat: fetch records dynamically

---
+++ javascript/exercise40/filtering-records.js
@@ -0,0 +1,21 @@
+import Database from "better-sqlite3";
+
+const db = new Database("./records.db", { fileMustExist: true })
+
+const search = "Ja"
+
+/** @type [{first_name: string, last_name: string, position: string, date: string}] rows */
+const rows = db.prepare("SELECT * FROM employees WHERE first_name like ?").all(`${search}%`)
+db.close()
+
+// console.log(rows)
+
+console.log(`${"Name".padEnd(20)} | ${"Position".padEnd(20)} | Separation Date`);
+console.log("-".repeat(60));
+for (const row of rows) {
+  const fullname = `${row.first_name} ${row.last_name}`.padEnd(20);
+  const position = row.position.padEnd(20)
+  const date = row.date || ""
+
+  console.log(`${fullname} | ${position} | ${date}`);
+}
