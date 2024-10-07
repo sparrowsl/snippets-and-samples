b0547f140852d1facbcc9064e285104689d703d3
sparrowsl <benthorpe.dev@gmail.com>
2024-10-07 16:50:24 +0000

    feat: add a basic endpoint for URL stats

--- go/exercise54-url-shortener/cmd/api/routes.go
+++ go/exercise54-url-shortener/cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -18,6 +19,11 @@ func (app *application) routes() http.Handler {
 	router.Get("/urls", app.getURLs)
 	router.Get("/urls/{id}", app.getURL)
 	router.Post("/urls", app.createURL)
+
+	router.Get("/urls/{id}/stats", func(w http.ResponseWriter, r *http.Request) {
+		message := fmt.Sprintf("stats for %s", chi.URLParam(r, "id"))
+		toJSON(w, http.StatusOK, map[string]any{"stats": message})
+	})
 
 	return router
 }
