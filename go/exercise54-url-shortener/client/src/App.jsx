1150b4b04df7885d42227f194d122832db7398de
sparrowsl <benthorpe.dev@gmail.com>
2024-10-07 17:32:31 +0000

    feat: display url card

--- go/exercise54-url-shortener/client/src/App.jsx
+++ go/exercise54-url-shortener/client/src/App.jsx
@@ -1,21 +1,35 @@
-import { createSignal } from "solid-js";
+import { For } from "solid-js";
 
-function App() {
-	const [count, setCount] = createSignal(0);
-
+export default function App() {
 	return (
 		<>
-			<section class="">
-				<h1>Breve - URL Shortner</h1>
+			<section class="max-w-5xl min-h-screen mx-auto bg-gray-50 py-5">
+				<h1 class="text-xl font-bold text-center underline">
+					Breve - URL Shortner
+				</h1>
 
-				<div>
-					<button type="button" onClick={() => setCount((count) => count + 1)}>
-						count is {count()}
-					</button>
+				<div class="grid grid-cols-3 gap-5 p-10">
+					<For each={Array.from({ length: 5 })}>
+						{<Card url={{ id: "", short_url: "short", long_url: "" }} />}
+					</For>
 				</div>
 			</section>
 		</>
 	);
 }
 
-export default App;
+/** @param {{url: {id:string, short_url:string, long_url:string}}} url  */
+function Card({ url }) {
+	return (
+		<>
+			<div class="bg-white shadow rounded p-3">
+				<h3>Short URL: &copy; {url.short_url}</h3>
+				<p>Long URL: &trade; {url.long_url}</p>
+
+				<a href="/" class="text-blue-800 block w-fit underline mt-5">
+					visit &rarr;
+				</a>
+			</div>
+		</>
+	);
+}
