fda152265458c4a0e61f25e764d85bb7158057cd
sparrowsl <benthorpe.dev@gmail.com>
2024-10-08 13:16:19 +0000

    feat: add a modal for adding new URL

--- go/exercise54-url-shortener/client/src/App.jsx
+++ go/exercise54-url-shortener/client/src/App.jsx
@@ -1,7 +1,9 @@
-import { createResource, For, Show } from "solid-js";
+import { createResource, createSignal, For, Show } from "solid-js";
 import NewUrl from "./NewUrl";
 
 export default function App() {
+	const [open, set_open] = createSignal(false);
+
 	const get_urls = async () => {
 		const res = await fetch("http://localhost:5000/urls");
 		const data = await res.json();
@@ -12,10 +14,33 @@ export default function App() {
 
 	return (
 		<>
-			<section class="max-w-5xl min-h-screen mx-auto bg-gray-50 py-5">
+			<section class="relative max-w-5xl min-h-screen mx-auto bg-gray-50 py-5">
 				<h1 class="text-xl font-bold text-center underline">
 					Breve - URL Shortner
 				</h1>
+				<button
+					type="button"
+					class="block w-fit text-xs mx-auto bg-blue-400 font-semibold px-4 py-2 cursor-pointer text-white rounded"
+					id="dialog"
+					onClick={() => set_open(true)}
+				>
+					Add URL
+				</button>
+
+				<Show when={open()}>
+					<div class="absolute inset-0 grid place-content-center bg-black/20">
+						<div class="bg-white p-10 rounded shadow relative min-w-96">
+							<button
+								type="button"
+								class="absolute top-1 right-3 text-2xl text-red-400 cursor-pointer"
+								onClick={() => set_open(false)}
+							>
+								&times;
+							</button>
+							<NewUrl />
+						</div>
+					</div>
+				</Show>
 
 				<Show when={data.loading}>
 					<p>loading urls....</p>
@@ -27,8 +52,6 @@ export default function App() {
 					</For>
 				</div>
 			</section>
-
-			<NewUrl />
 		</>
 	);
 }
@@ -43,7 +66,8 @@ function Card({ url }) {
 					<span class="underline text-blue-500">{url.short_url}</span>
 				</h3>
 				<p class="text-sm">
-					Long URL: <span class="italic text-gray-500">{url.long_url}</span>
+					Long URL:{" "}
+					<span class="italic line-clamp-1 text-gray-500">{url.long_url}</span>
 				</p>
 
 				<div class="flex items-center justify-between mt-5">
--- go/exercise54-url-shortener/client/src/NewUrl.jsx
+++ go/exercise54-url-shortener/client/src/NewUrl.jsx
@@ -10,27 +10,35 @@ export default function NewUrl() {
 			return;
 		}
 
-		await fetch("http://localhost:5000/urls", {
+		const res = await fetch("http://localhost:5000/urls", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({ url: url() }),
 		});
+		if (!res.ok) {
+			return;
+		}
+
+		// reload window if success
+		window.location.reload();
 	};
 
 	return (
 		<>
 			<section>
-				<form action="" method="POST" onSubmit={add_url}>
+				<form method="POST" onSubmit={add_url}>
+					<legend class="text-center font-semibold text-sm">Add New URL</legend>
+
 					<input
 						type="text"
 						name="long_url"
 						placeholder="https://www.google.com"
-						class="block text-sm rounded p-1"
+						class="block text-sm rounded p-2 w-full my-5"
 						onChange={(e) => set_url(e.target.value)}
 					/>
 					<button
 						type="submit"
-						class="block bg-gray-300 text-sm rounded max-w-fit py-2 px-6"
+						class="cursor-pointer mx-auto hover:bg-blue-400 block bg-gray-300 text-sm rounded max-w-fit py-2 px-6"
 					>
 						Submit
 					</button>
