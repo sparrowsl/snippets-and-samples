8b69694e0ebe248d95ce9ceb84a6ba0ecb9b369d
sparrowsl <benthorpe.dev@gmail.com>
2024-12-05 17:46:05 +0000

    chore: find employees

--- c/exercise34/employee_list_removal.c
+++ c/exercise34/employee_list_removal.c
@@ -1,31 +1,61 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void remove_employee(char *, char *);
+void display_employees(char *[], int);
+void remove_employee(char *[], char *, int);
+bool find_employee(char *[], char *, int);
 
 char *names[] = {"John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen",
                  "Jeremy Goodwin"};
 
 int main(void) {
+  int array_length = sizeof(names) / sizeof(names[0]);
   char remove[15];
 
-  int names_length = (int)(sizeof(names) / sizeof(names[0]));
-
-  printf("There are %d employees:\n", names_length);
-  for (int i = 0; i < names_length; i++) {
-    printf("- %s\n", names[i]);
-  }
+  display_employees(names, array_length);
 
   printf("Enter an employee name to remove: ");
-  scanf("%s", remove);
+  fgets(remove, 15, stdin);
+  remove[strcspn(remove, "\n")] = 0;
 
+  /*if (find_employee(names, remove, initial_names_length)) {*/
+  /*} else {*/
+  /*  puts("No employee name found");*/
+  /*}*/
+  remove_employee(names, remove, array_length);
+
+  printf("Exist: %d\n", find_employee(names, remove, array_length));
+
+  array_length = sizeof(names) / sizeof(names[0]);
+
+  display_employees(names, array_length);
   return EXIT_SUCCESS;
 }
 
-void remove_employee(char *array, char *name) {
-  int names_length = (int)(sizeof(names) / sizeof(names[0]));
-
-  for (int i = 0; i < names_length; i++) {
+void display_employees(char *employees[], int len) {
+  printf("There are %d employees:\n", len);
+  for (int i = 0; i < len; i++) {
+    printf("- %s\n", employees[i]);
   }
+}
+
+void remove_employee(char *array[], char *name, int length) {
+  for (int i = 0; i < length; i++) {
+    if (!strcmp(array[i], name)) {
+      printf("found %s\n", name);
+      array[i] = array[i + 1];
+    }
+  }
+}
+
+bool find_employee(char *employees[], char *name, int length) {
+  for (int i = 0; i < length; i++) {
+    if (!strcmp(employees[i], name)) {
+      return true;
+    }
+  }
+
+  return false;
 }
