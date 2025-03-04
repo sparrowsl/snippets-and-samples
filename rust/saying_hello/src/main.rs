b32eb0015221bab4984cdf3ac1a04ce144b1f74d
Sparrow <benjaminthorpe19@gmail.com>
2025-03-04 18:38:01 +0000

    feat: read user input

--- rust/saying_hello/src/main.rs
+++ rust/saying_hello/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Write};
+
 fn main() {
     print!("What is your name? ");
+    io::stdout().flush().unwrap(); // Flush output to ensure prompt appears before input
+
+    let mut name = String::new();
+    _ = io::stdin().read_line(&mut name);
+
+    println!("Hello, {}, nice to meet you!", name.trim());
 }
