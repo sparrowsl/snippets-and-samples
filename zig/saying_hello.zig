4a0db4493f40f24136705609490aaeb078328708
Sparrow <benjaminthorpe19@gmail.com>
2025-03-06 15:41:00 +0000

    say hello in zig

---
+++ zig/saying_hello.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    var buffer: [20]u8 = undefined;
+
+    std.debug.print("What is your name? ", .{});
+    const name = try stdin.readUntilDelimiter(buffer[0..], '\n');
+
+    std.debug.print("Hello, {s}, nice to meet you!", .{name});
+}
