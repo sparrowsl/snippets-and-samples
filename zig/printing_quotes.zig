68f27a94d90ee24ddaba5625a98c40b5a1a8cfba
Sparrow <benjaminthorpe19@gmail.com>
2025-03-07 16:19:02 +0000

    chore: rewrote buffer

--- zig/printing_quotes.zig
+++ zig/printing_quotes.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
-    var quote_buffer: [200]u8 = undefined;
+    var buffer: [255]u8 = undefined;
 
     std.debug.print("What is the quote? ", .{});
-    const quote = try stdin.readUntilDelimiter(quote_buffer[0..], '\n');
+    const quote = try stdin.readUntilDelimiter(buffer[0..], '\n');
 
-    var author_buffer: [30]u8 = undefined;
     std.debug.print("Who said it? ", .{});
-    const author = try stdin.readUntilDelimiter(author_buffer[0..], '\n');
+    const author = try stdin.readUntilDelimiter(buffer[quote.len..], '\n');
 
     std.debug.print("{s} says, \"{s}\"", .{ author, quote });
 }
