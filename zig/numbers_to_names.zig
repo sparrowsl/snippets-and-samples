40f6cdf81c4c0009448da7a55bd227d036da4409
Sparrow <benjaminthorpe19@gmail.com>
2025-03-17 17:06:57 +0000

    numbers to names

---
+++ zig/numbers_to_names.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    var buffer: [5]u8 = undefined;
+
+    std.debug.print("Please enter the number of the month: ", .{});
+    const input = try stdin.readUntilDelimiter(buffer[0..], '\n');
+
+    const month_idx = try std.fmt.parseInt(i32, input, 10);
+
+    const month_name = switch (month_idx) {
+        1 => "January",
+        2 => "February",
+        3 => "March",
+        4 => "April",
+        5 => "May",
+        6 => "June",
+        7 => "July",
+        8 => "August",
+        9 => "September",
+        10 => "October",
+        11 => "November",
+        12 => "December",
+        else => "Invalid Month",
+    };
+
+    std.debug.print("The name of the month is {s}", .{month_name});
+}
